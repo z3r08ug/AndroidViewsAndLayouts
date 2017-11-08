58563c099a637dd0e58fb29bac22b8f84a1a8305
Chris Vansco <chris.vansco@gmail.com>
2017-11-08 11:27:46 -0500

    changed first activity to basic login page

--- app/src/main/java/dev/uublabs/androidviewsandlayouts/MainActivity.java
+++ app/src/main/java/dev/uublabs/androidviewsandlayouts/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity
 
     private WebView wv;
     private static final String TAG = "MainActivityTAG";
+    private EditText etFirstName;
+    private EditText etLastName;
+    private TextView tvPersonName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,7 +26,14 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bindViews();
+    }
 
+    private void bindViews()
+    {
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        tvPersonName = findViewById(R.id.tvPersonName);
     }
 
     public void goToSecondActivity(View view)
